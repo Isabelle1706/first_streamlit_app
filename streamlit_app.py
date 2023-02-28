4d3e35630d2ede43e5d8319bf3c8aa298b54c83b
Isabelle1706 <xiaoqingyou@hotmail.com>
2023-02-28 10:05:58 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,5 +58,5 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-streamlit.text(fruityvice_response)
+streamlit.dataframe(fruityvice_response)
 
