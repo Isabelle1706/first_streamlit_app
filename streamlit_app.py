2a7c1a4cab349b1e32787cf76a021fdeeac606d0
Isabelle1706 <xiaoqingyou@hotmail.com>
2023-02-28 09:47:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# take the json version of response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output the screen as a table
+streamlit.dataframe(fruityvice_normalized)
+
+
+
