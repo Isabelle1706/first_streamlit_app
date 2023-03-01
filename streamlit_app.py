fff4ddf907205da0d22c2a18d206cf6cfacdf501
Isabelle1706 <xiaoqingyou@hotmail.com>
2023-03-01 15:55:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ streamlit.text(fruityvice_response)
 #    streamlit.error()
     
 # create a function
-def get_fruityvice_date(this_fruit_choice):
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
@@ -67,7 +67,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else: 
-    back_from_function=get_fruityvice_date(fruit_choice)
+    back_from_function=get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
     
     
