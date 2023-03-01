c153bf7fa6aad562514d1b5fe265b3110d0fa7f1
Isabelle1706 <xiaoqingyou@hotmail.com>
2023-03-01 15:52:22 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ try:
     streamlit.dataframe(back_from_function)
     
     
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
