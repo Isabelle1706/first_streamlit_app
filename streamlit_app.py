685d9330689ad45e24349659c11f10d841a9a2d3
Isabelle1706 <xiaoqingyou@hotmail.com>
2023-03-01 14:37:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/")
-add_my_fruit=streamlit.text_input("What fruit would you like to add?)
+add_my_fruit=streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)                                  
 
 
